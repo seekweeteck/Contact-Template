71a84ab27e8db811ca599cea307d34246a65f2af
See Kwee Teck <seekt@tarc.edu.my>
2023-08-24 17:35:45 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/mycontact/ui/add_contact/AddContactFragment.kt
@@ -0,0 +1,67 @@
+package my.edu.tarc.mycontact.ui.add_contact
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import my.edu.tarc.mycontact.R
+import my.edu.tarc.mycontact.databinding.FragmentAddContactBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class AddContactFragment : Fragment(), MenuProvider {
+
+    private var _binding: FragmentAddContactBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddContactBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSave.setOnClickListener {
+            findNavController().navigate(R.id.action_AddContactFragment_to_ContactListFragment)
+        }
+
+        //Add support for menu
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.findItem(R.id.action_settings).setVisible(false)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if(menuItem.itemId == android.R.id.home){
+            findNavController().navigateUp()
+        }
+
+        return true
+    }
+}
